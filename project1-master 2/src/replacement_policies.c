c367373de1a5438c3bfe2071ed57f8f2967c32a2
Shadi-Nrz <shadinourriz@mines.edu>
2025-03-07 14:03:39 -0700

    Update replacement_policies.c

--- project1-master 2/src/replacement_policies.c
+++ project1-master 2/src/replacement_policies.c
@@ -17,25 +17,62 @@
 // LRU Replacement Policy
 // ============================================================================
 // TODO feel free to create additional structs/enums as necessary
+// Shadi's code
+struct lru_data {
+	uint32_t num_lines;
+	uint32_t current_time;
+	uint32_t *access_times;
+};
 
 void lru_cache_access(struct replacement_policy *replacement_policy,
                       struct cache_system *cache_system, uint32_t set_idx, uint32_t tag)
 {
     // TODO update the LRU replacement policy state given a new memory access
+    // Shadi's code
+    struct lru_data *data = (struct lru_data *)replacement_policy->data;
+  	data->current_time++;
+  	for (
+      			uint32_t i = set_idx * cache_system->associativity;
+      			i < (set_idx+1) * cache_system->associativity;
+      			i++) {
+    		if (cache_system->cache_lines[i].tag == tag) {
+      			data->access_times[i] = data->current_time;
+      			break;
+  		}
+  	}
 }
 
 uint32_t lru_eviction_index(struct replacement_policy *replacement_policy,
                             struct cache_system *cache_system, uint32_t set_idx)
 {
     // TODO return the index within the set that should be evicted.
-
-    return 0;
+    // Shadi's code
+    struct lru_data *data = (struct lru_data *)replacement_policy->data;
+  
+  	uint32_t index;
+  	uint32_t min_time;
+  	bool min_time_set = false;
+  	for (
+      			uint32_t i = set_idx * cache_system->associativity;
+      			i < (set_idx+1) * cache_system->associativity;
+      			i++) {
+    		if (data->access_times[i] < min_time || !min_time_set) {
+      			min_time_set = true;
+      			min_time = data->access_times[i];
+      			index = i;
+  		  }
+  	}
+      return index % cache_system->associativity;
+    // return 0;
 }
 
 void lru_replacement_policy_cleanup(struct replacement_policy *replacement_policy)
 {
     // TODO cleanup any additional memory that you allocated in the
     // lru_replacement_policy_new function.
+    // Shadi's code
+    free(((struct lru_data *)replacement_policy->data)->access_times);
+  	free(replacement_policy->data);
 }
 
 struct replacement_policy *lru_replacement_policy_new(uint32_t sets, uint32_t associativity)
@@ -47,6 +84,11 @@ struct replacement_policy *lru_replacement_policy_new(uint32_t sets, uint32_t as
 
     // TODO allocate any additional memory to store metadata here and assign to
     // lru_rp->data.
+    // Shadi's code
+    struct lru_data *data = calloc(1, sizeof(struct lru_data));
+  	data->num_lines = sets * associativity;
+  	data->access_times = calloc(data->num_lines, sizeof(uint32_t));
+    lru_rp->data = data;
 
     return lru_rp;
 }
@@ -57,20 +99,23 @@ void rand_cache_access(struct replacement_policy *replacement_policy,
                        struct cache_system *cache_system, uint32_t set_idx, uint32_t tag)
 {
     // TODO update the RAND replacement policy state given a new memory access
+    // Shadi's code
 }
 
 uint32_t rand_eviction_index(struct replacement_policy *replacement_policy,
                              struct cache_system *cache_system, uint32_t set_idx)
 {
     // TODO return the index within the set that should be evicted.
-
-    return 0;
+    // Shadi's code
+    //return 0;
+    return rand() % cache_system->associativity;
 }
 
 void rand_replacement_policy_cleanup(struct replacement_policy *replacement_policy)
 {
     // TODO cleanup any additional memory that you allocated in the
     // rand_replacement_policy_new function.
+    // Shadi's code
 }
 
 struct replacement_policy *rand_replacement_policy_new(uint32_t sets, uint32_t associativity)
@@ -85,7 +130,7 @@ struct replacement_policy *rand_replacement_policy_new(uint32_t sets, uint32_t a
 
     // TODO allocate any additional memory to store metadata here and assign to
     // rand_rp->data.
-
+    // Shadi's code
     return rand_rp;
 }
 
@@ -98,20 +143,46 @@ void lru_prefer_clean_cache_access(struct replacement_policy *replacement_policy
     // TODO update the LRU_PREFER_CLEAN replacement policy state given a new
     // memory access
     // NOTE: you may be able to share code with the LRU policy
+    // Shadi's code
+    lru_cache_access(replacement_policy, cache_system, set_idx, tag);
 }
 
 uint32_t lru_prefer_clean_eviction_index(struct replacement_policy *replacement_policy,
                                          struct cache_system *cache_system, uint32_t set_idx)
 {
     // TODO return the index within the set that should be evicted.
+    // Shadi's code
+    // return 0;
+    struct lru_data *data = (struct lru_data *)replacement_policy->data;
 
-    return 0;
+  	uint32_t index;
+  	uint32_t min_time;
+  	bool min_time_set = false;
+  	for (
+      			uint32_t i = set_idx * cache_system->associativity;
+      			i < (set_idx+1) * cache_system->associativity;
+      			i++) {
+    		if (cache_system->cache_lines[i].status != MODIFIED) {
+    			if (data->access_times[i] < min_time || !min_time_set) {
+    				min_time_set = true;
+    				min_time = data->access_times[i];
+    				index = i;
+    			}
+    		}
+  	}
+  	if (!min_time_set) {
+    		return lru_eviction_index(replacement_policy, cache_system, set_idx);
+  	} else {
+  	    return index % cache_system->associativity;
+  	}
 }
 
 void lru_prefer_clean_replacement_policy_cleanup(struct replacement_policy *replacement_policy)
 {
     // TODO cleanup any additional memory that you allocated in the
     // lru_prefer_clean_replacement_policy_new function.
+    // Shadi's code
+    lru_replacement_policy_cleanup(replacement_policy);
 }
 
 struct replacement_policy *lru_prefer_clean_replacement_policy_new(uint32_t sets,
@@ -124,6 +195,11 @@ struct replacement_policy *lru_prefer_clean_replacement_policy_new(uint32_t sets
 
     // TODO allocate any additional memory to store metadata here and assign to
     // lru_prefer_clean_rp->data.
+    // Shadi's code
+    struct lru_data *data = calloc(1, sizeof(struct lru_data));
+  	data->num_lines = sets * associativity;
+  	data->access_times = calloc(data->num_lines, sizeof(uint32_t));
+    lru_prefer_clean_rp->data = data;
 
     return lru_prefer_clean_rp;
 }
